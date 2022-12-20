31921affc1096a1ddda2bd2350844b77d2e36a17
Ahmed Adel Alemam <ahmedemam755@gmail.com>
2022-12-20 13:39:03 +0200

    Updates

---
+++ precache-manifest.443f33f0cde2c6731e606f510e6455aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee70050c3651c8584c57c5ca6532a803",
+    "url": "/AhmedAdelAlemam/my-online-portfolio/index.html"
+  },
+  {
+    "revision": "3999ce4b3465e3b83eb8",
+    "url": "/AhmedAdelAlemam/my-online-portfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "30cdfa4b452ef332464d",
+    "url": "/AhmedAdelAlemam/my-online-portfolio/static/css/main.8e832286.chunk.css"
+  },
+  {
+    "revision": "3999ce4b3465e3b83eb8",
+    "url": "/AhmedAdelAlemam/my-online-portfolio/static/js/2.ba089c60.chunk.js"
+  },
+  {
+    "revision": "30cdfa4b452ef332464d",
+    "url": "/AhmedAdelAlemam/my-online-portfolio/static/js/main.470a1d14.chunk.js"
+  },
+  {
+    "revision": "3d54af57b62435f394ff",
+    "url": "/AhmedAdelAlemam/my-online-portfolio/static/js/runtime~main.61c32dd0.js"
+  }
+]);
